1f6973d8f58a1d328b7c7d36b01836f72690ff24
Trịnh Đình Thạnh <108230635+thanhtd273@users.noreply.github.com>
2024-11-19 16:59:13 +0000

    Split code to smaller function

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ class HotelService:
         result: list[Hotel] = []
         length = len(data)
         for i in range(length):
-            id = data[i].id
+            hotel_id = data[i].id
             destination_id = data[i].destination_id
-            index = Util.find_index(result, id, destination_id)
+            index = Util.find_index(result, hotel_id, destination_id)
             if index == -1:
                 result.append(data[i])
                 continue
@@ -30,55 +30,17 @@ class HotelService:
             existed = result[index]
             if data[i].name is not None:
                 existed.name = data[i].name
-
             if data[i].description is not None:
                 existed.description = data[i].description
 
-            if existed.location is None:
-                existed.location = data[i].location
-            else:
-                location = data[i].location
-                if location.address is not None:
-                    existed.location.address = location.address
-                if location.city is not None:
-                    existed.location.city = location.city
-                if location.country is not None:
-                    existed.location.country = location.country
-                if location.lat is not None:
-                    existed.location.lat = location.lat
-                if location.lng is not None:
-                    existed.location.lng = location.lng
-
-            if existed.amenities is None:
-                existed.amenities = data[i].amenities
-            elif data[i].amenities is not None:
-                general = data[i].amenities.general
-                if general is not None:
-                    existed.amenities.general = Util.merge_slist(
-                        existed.amenities.general, general)
-
-                room = data[i].amenities.room
-                if room is not None:
-                    existed.amenities.room = Util.merge_slist(
-                        existed.amenities.room, room)
-
-            if existed.images is None:
-                existed.images = data[i].images
-            elif data[i].images is not None:
-                existed.images.rooms = Util.merge_list(existed.images.rooms,
-                                                       data[i].images.rooms)
-                existed.images.site = Util.merge_list(existed.images.site,
-                                                      data[i].images.site)
-                existed.images.amenities = Util.merge_list(
-                    existed.images.amenities, data[i].images.amenities)
-
-            if existed.booking_conditions is None or len(
-                    existed.booking_conditions) == 0:
-                existed.booking_conditions = data[i].booking_conditions
-            elif data[i].booking_conditions is not None and len(
-                    data[i].booking_conditions) != 0:
-                existed.booking_conditions = Util.merge_list(
-                    existed.booking_conditions, data[i].booking_conditions)
+            existed.location = HotelService.merge_location(
+                existed.location, data[i].location)
+            existed.amenities = HotelService.merge_amenities(
+                existed.amenities, data[i].amenities)
+            existed.images = HotelService.merge_images(existed.images,
+                                                       data[i].images)
+            existed.booking_conditions = HotelService.merge_booking_condition(
+                existed.booking_conditions, data[i].booking_conditions)
 
         return result
 
@@ -92,6 +54,56 @@ class HotelService:
                 result.append(info)
 
         return result
+
+    @staticmethod
+    def merge_location(origin, new):
+        if origin is None:
+            origin = new
+        else:
+            if new.address is not None:
+                origin.address = new.address
+            if new.city is not None:
+                origin.city = new.city
+            if new.country is not None:
+                origin.country = new.country
+            if new.lat is not None:
+                origin.lat = new.lat
+            if new.lng is not None:
+                origin.lng = new.lng
+        return origin
+
+    @staticmethod
+    def merge_amenities(origin, new):
+        if origin is None:
+            origin = new
+        elif new is not None:
+            general = new.general
+            if general is not None:
+                origin.general = Util.merge_slist(origin.general, general)
+
+            room = new.room
+            if room is not None:
+                origin.room = Util.merge_slist(origin.room, room)
+        return origin
+
+    @staticmethod
+    def merge_images(origin, data):
+        if origin is None:
+            return data
+        elif data is not None:
+            origin.rooms = Util.merge_list(origin.rooms, data.rooms)
+            origin.site = Util.merge_list(origin.site, data.site)
+            origin.amenities = Util.merge_list(origin.amenities,
+                                               data.amenities)
+        return origin
+
+    @staticmethod
+    def merge_booking_condition(origin, data):
+        if origin is None or len(origin) == 0:
+            return data
+        elif data is not None and len(data) != 0:
+            origin = Util.merge_list(origin, data)
+        return origin
 
 
 def fetch_hotels(hotel_ids: list[str], destination_ids: list[str]):
